6f79528a07b0b5e1cfea87ae62dfcbe848621221
Chen <chuancc@88e9fe51b7a2.ant.amazon.com>
2019-08-27 12:59:36 +0800

    fixed bug - principal service DNS issue.

--- bin/install_all.sh
+++ bin/install_all.sh
@@ -7,5 +7,16 @@ echo "Done"
 echo "Setup infrastructure layer......"
 ./infra.sh
 
+if [ $? -ne 0 ]
+then
+    echo "Setup infrastructure layer was failed and exit building process."
+    exit 0
+fi 
+
 echo "Setup ECS cluster......"
 ./ecs.sh $1
+
+if [ $? -ne 0 ]
+then
+    echo "Setup ECS cluster was failed and check out logs/console."
+fi 
